ce4245cfd5d7b7c9721c8580ad354387694e81a1
Xom1bc <55351194+Xom1bc@users.noreply.github.com>
2021-01-23 14:34:23 +0000

    Made my rocket

--- js/myStuff.js
+++ js/myStuff.js
@@ -1,5 +1,16 @@
+// this function does this.
+var doCoolStuff = function () {
+	var currentClassName = document.getElementById("cool").className;
+
+	if (currentClassName == "cool") {
+		document.getElementById("cool").className = "cool red";
+	} else {
+		document.getElementById("cool").className = "cool"
+	}
+}
+
 var sayMyName = function (name) {
-	alert("My name is "+name);
+	alert("My name is: "+name);
 }
 
 
