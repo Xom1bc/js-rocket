826fb8debd73290ebfbad50e892d45d992a7fecf
Xom1bc <55351194+Xom1bc@users.noreply.github.com>
2021-01-24 11:51:58 +0000

    Made our rocket cool with join etc.

--- js/rocket.js
+++ js/rocket.js
@@ -12,6 +12,21 @@ var changeState = function (state) {
 		timer = setInterval(function () {
 			countdownNumber = countdownNumber -1;
 			document.getElementById("countdown").innerHTML = countdownNumber;
+
+			if (countdownNumber > 4  && countdownNumber <= 7) {
+				// be nervous
+				document.getElementById("nervous").className = "nervous show";
+			} else{
+				document.getElementById("nervous").className = "nervous";
+			}
+
+			if (countdownNumber > 1  && countdownNumber <= 4) {
+				// cant wait
+				document.getElementById("cant-wait").className = "cant-wait show";
+			} else{
+				document.getElementById("cant-wait").className = "cant-wait";
+			}
+
 			if (countdownNumber <= 0) {
 				changeState(3);
 			};
